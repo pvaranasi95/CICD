f031826e16a34ccad666ef5867ea48d44b404968
pvaranasi95 <98229537+pvaranasi95@users.noreply.github.com>
2026-03-15 16:59:01 +0530

    Update Jenkins_Job_Creation.groovy

--- Groovy_Scripts/Jenkins_Job_Creation.groovy
+++ Groovy_Scripts/Jenkins_Job_Creation.groovy
@@ -1,5 +1,7 @@
 pipeline {
-    agent any
+    agent {
+        label 'Windows_Agent'
+    }
     stages {
         stage('Git checkout') {
             steps {
@@ -14,36 +16,21 @@ pipeline {
             }
         }
 
-        // stage('Jenkins_cli.jar Download') {
-        //     steps {
-        //         script {
-        //             // Ensure local path exists
-        //             sh "curl -O http://localhost:8080/jnlpJars/jenkins-cli.jar"
-        //             echo "jenkins-cli.jar downloaded"
+        stage('Jenkins_cli.jar Download') {
+            steps {
+                script {
+                    // Ensure local path exists
+                    sh "curl.exe -O http://localhost:8080/jnlpJars/jenkins-cli.jar"
+                    echo "jenkins-cli.jar downloaded"
 
-        //         }
-        //     }
-        // }
+                }
+            }
+        }
       stage("Jenkins_Job_Creation") {
             steps {
               script {
-                def xmlFile
-
-                    if (params.Type == 'Pipeline') {
-                        xmlFile = 'pipeline.xml'
-                    } else if (params.Type == 'Freestyle') {
-                        xmlFile = 'freestyle.xml'
-                    } else if (params.Type == 'Multi-configuration') {
-                        xmlFile = 'Multi-configuration.xml'
-                    } else if (params.Type == 'Multi-Branch') {
-                        xmlFile = 'Multi-Branch.xml'
-                    } else if (params.Type == 'Folder') {
-                        xmlFile = 'Folder.xml'
-                    } else {
-                        xmlFile = 'organization.xml'
-                    }
                             
-                sh """curl -X POST -u pvaranasi95:11f5916d329915b0f7e7df158d546c5eff -H "Content-Type: application/xml" --data-binary @Pipeline_Creation_XML/${xmlFile} http://localhost:8080/createItem?name=${params.Job_Name}"""
+               bat """java -jar jenkins-cli.jar -auth pvaranasi95:Pvaranasi95 -s http://localhost:8080/ create-job ${params.Job_Name} --file Pipeline_Creation_XML//${params.Type}.xml"""
               }
             }
       }
