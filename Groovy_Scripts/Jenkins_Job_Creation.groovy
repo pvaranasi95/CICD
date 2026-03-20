571733e2f6268860bb5f62070da31cc775aa1f5d
pvaranasi95 <98229537+pvaranasi95@users.noreply.github.com>
2026-03-20 16:55:13 +0530

    Update Jenkins_Job_Creation.groovy

--- Groovy_Scripts/Jenkins_Job_Creation.groovy
+++ Groovy_Scripts/Jenkins_Job_Creation.groovy
@@ -37,21 +37,20 @@ pipeline {
             script {
 
                 sh '''
-                    java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ get-view ${params.APP_CODE}
-                    '''
-                //     if [ "$CHECK_VIEW" == "${params.APP_CODE}" ]; then
-                //          echo "View already exists. Adding new job to ${params.APP_CODE} view."
-                                    // CHECK_VIEW=$(java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ get-view ${params.APP_CODE})
-                          
-                //     else
-                //         echo "View not exists. Creating new view"
-                //         java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ create-view < Pipeline_Creation_XML/view.xml
-                //     fi   
+                    java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ get-view ${params.APP_CODE} > ${params.APP_CODE}.txt
+
+                    if [ -f "${params.APP_CODE}.txt" ]; then
+                         echo "View already exists. Adding new job to ${params.APP_CODE} view."
+      
+                    else
+                        echo "View not exists. Creating new view"
+                        java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ create-view < Pipeline_Creation_XML/view.xml
+                    fi   
 
 
-                // echo "Adding Job To View"
-                // java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ add-job-to-view ${params.APP_CODE} ${params.Job_Name}
-                // '''
+                echo "Adding Job To View"
+                java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ add-job-to-view ${params.APP_CODE} ${params.Job_Name}
+                '''
                 }
             }
         }
