17ea32a5abd626a578cff5ef71dace0acb69a3bf
pvaranasi95 <98229537+pvaranasi95@users.noreply.github.com>
2026-03-20 16:47:50 +0530

    Update Jenkins_Job_Creation.groovy

--- Groovy_Scripts/Jenkins_Job_Creation.groovy
+++ Groovy_Scripts/Jenkins_Job_Creation.groovy
@@ -37,11 +37,11 @@ pipeline {
             script {
 
                 sh '''
-                    // CHECK_VIEW=$(java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ get-view ${params.APP_CODE})
                     java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ get-view ${params.APP_CODE}
                     '''
                 //     if [ "$CHECK_VIEW" == "${params.APP_CODE}" ]; then
                 //          echo "View already exists. Adding new job to ${params.APP_CODE} view."
+                                    // CHECK_VIEW=$(java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ get-view ${params.APP_CODE})
                           
                 //     else
                 //         echo "View not exists. Creating new view"
