22d08d1ed641b9691d2f4f117d9b3e3c5360bf19
pvaranasi95 <98229537+pvaranasi95@users.noreply.github.com>
2026-04-19 11:12:01 +0530

    Update Jenkins_Job_Creation.groovy

--- Groovy_Scripts/Jenkins_Job_Creation.groovy
+++ Groovy_Scripts/Jenkins_Job_Creation.groovy
@@ -18,7 +18,7 @@ pipeline {
             steps {
                 script {
                     // Ensure local path exists
-                    sh "curl -O http://localhost:8080/jnlpJars/jenkins-cli.jar"
+                    sh "curl -O http://localhost:8088/jnlpJars/jenkins-cli.jar"
                     echo "jenkins-cli.jar downloaded"
 
                 }
@@ -28,7 +28,7 @@ pipeline {
             steps {
               script {
                 sh "cd .."         
-               sh """java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ create-job ${params.Job_Name} < Pipeline_Creation_XML/pipeline.xml"""
+               sh """java -jar jenkins-cli.jar -http -auth pavanvaranasi95:113f3314b38a6655edfc5b375331f47767 -s http://localhost:8088/ create-job ${params.Job_Name} < Pipeline_Creation_XML/pipeline.xml"""
               }
             }
       }
@@ -41,13 +41,13 @@ stage("Add Jobs to View") {
             def app_status = params.APP_CODE_STATUS
             sh """  
                 if [ "${app_status}" = "NEW" ]; then
-                   java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ create-view ${app} < Pipeline_Creation_XML/view.xml
+                   java -jar jenkins-cli.jar -http -auth pavanvaranasi95:113f3314b38a6655edfc5b375331f47767 -s http://localhost:8088/ create-view ${app} < Pipeline_Creation_XML/view.xml
                    echo "${app} view created."
-                   java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ add-job-to-view ${app} ${job}
+                   java -jar jenkins-cli.jar -http -auth pavanvaranasi95:113f3314b38a6655edfc5b375331f47767 -s http://localhost:8088/ add-job-to-view ${app} ${job}
                    echo "${job} added to ${app} view"
                 else
                    echo "Adding Job To Existing View"
-                   java -jar jenkins-cli.jar -http -auth pavanvaranasi95:11fa7390e7a1b0114123e7034528793f9f -s http://localhost:8080/ add-job-to-view ${app} ${job}
+                   java -jar jenkins-cli.jar -http -auth pavanvaranasi95:113f3314b38a6655edfc5b375331f47767 -s http://localhost:8088/ add-job-to-view ${app} ${job}
                 fi
             """
         }
