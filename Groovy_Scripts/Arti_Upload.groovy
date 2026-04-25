873dd4bd85f8018efa9bb8ba574194df9c81ad12
pvaranasi95 <98229537+pvaranasi95@users.noreply.github.com>
2026-04-25 14:39:59 +0530

    Create Arti_Upload.groovy

---
+++ Groovy_Scripts/Arti_Upload.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent { label 'Windows' }
+    environment {
+        ARTIFACTORY_CRED = credentials('Jfrog_Artifactory')
+    }
+    stages {
+        stage('Git checkout') {
+            steps {
+                checkout scmGit(
+                    branches: [[name: '*/main']],
+                    extensions: [],
+                    userRemoteConfigs: [[
+                        url: 'https://github.com/pvaranasi95/CICD.git',
+                        credentialsId: 'GitHub_Cred'
+                    ]]
+                )
+            }
+        }
+
+        stage('Artifactory Upload') {
+            steps {
+                script {
+                    // Ensure local path exists
+                    // Download using JFrog CLI
+                    bat """
+                    curl.exe -u admin:password -T "${params.Source_File_Path}" "http://localhost:8082/${params.Artifactory_Target_Folder}/"
+                    
+                    """
+                }
+                echo "Artifacts uploaded successfully to ${params.Artifactory_Target_Folder}"
+            }
+        }
+    }
+}
